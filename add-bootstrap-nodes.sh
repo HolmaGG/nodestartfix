808409641081b8e4c886c9c658472c5fd4cfae1a
HolmaGG <101505618+HolmaGG@users.noreply.github.com>
2022-04-09 19:53:21 +0300

    Update add-bootstrap-nodes.sh

--- add-bootstrap-nodes.sh
+++ add-bootstrap-nodes.sh
@@ -10,5 +10,3 @@ bootstrap_list = [
 	["194.233.95.166:31245, "6eixWUbvxSTBDReVjUPzFc2yp7HT5k2KsAENkf1YeBAJ2vwzmE"]
 > massa/massa-node/config/config.toml
 systemctl restart massad
-echo "fixed applied, wait a few minutes and check logs:"
-echo "journalctl -n 100 -f -u massa"
