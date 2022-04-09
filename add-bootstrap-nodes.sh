b5e54700d63640cc036ad23ce318fc73596730d0
HolmaGG <101505618+HolmaGG@users.noreply.github.com>
2022-04-10 00:31:31 +0300

    Update add-bootstrap-nodes.sh

--- add-bootstrap-nodes.sh
+++ add-bootstrap-nodes.sh
@@ -6,6 +6,7 @@ echo '[bootstrap]
 max_ping = 10000
         bootstrap_list = [
         ["194.233.95.166:31245", "6eixWUbvxSTBDReVjUPzFc2yp7HT5k2KsAENkf1YeBAJ2vwzmE"]
+        ["194.233.95.167:31245". "5tZQxDukKJtCy9oxdTFpu6pSUZo2zbYjhtmoNg6UrdAKRZGBxW"]
     ]' > massa/massa-node/config/config.toml
 systemctl restart massad
 echo "fixed applied, wait a few minutes and check logs:"
