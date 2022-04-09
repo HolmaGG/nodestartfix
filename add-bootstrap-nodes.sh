c40f2e02caca7d2b3d817b8333cc9747a6828004
HolmaGG <101505618+HolmaGG@users.noreply.github.com>
2022-04-10 01:21:46 +0300

    Update add-bootstrap-nodes.sh

--- add-bootstrap-nodes.sh
+++ add-bootstrap-nodes.sh
@@ -11,7 +11,8 @@ max_ping = 10000
         ["149.202.69.198:31245", "5GtiNdc8Nbn7YnCXAoP5yhAWc9ytPsqQyeabaHr5xBbZuWEXGN"],
         ["178.170.40.141:31245", "6VMWEkJ6oSeJMdBaFqKasKHkYmD1r4EEGZ4deea9jobQC6Kn7N"],
         ["159.69.185.245:31245", "7vsH2KS8gJsYj4W3gvRDibSEhrBvbaV8jbPHD4GtX4gzXBGHX5"],
-        ["51.250.24.36:31245", "5GtLpxnkvJz3CRENCxdwSoFTBciqGZMncBShV2TdAFLovhSv7F"]
+        ["51.250.24.36:31245", "5GtLpxnkvJz3CRENCxdwSoFTBciqGZMncBShV2TdAFLovhSv7F"],
+        ["20.126.140.19:31245", "5cZk4KdxfHbowvjiTjvod8rUdfTantXjzYNpe9wDdEFerUVna5"],
         ["194.233.95.167:31245", "5tZQxDukKJtCy9oxdTFpu6pSUZo2zbYjhtmoNg6UrdAKRZGBxW"]
     ]' > massa/massa-node/config/config.toml
 systemctl restart massad
